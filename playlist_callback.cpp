386fe73c2701a2a16e7ace31f9b7256a23061cc6
Saxon Landers <saxon@ackwell.com.au>
2013-12-15 21:06:29 +1100

    Now picks up dupe playlist names

--- playlist_callback.cpp
+++ playlist_callback.cpp
@@ -2,10 +2,24 @@
 
 // Called when a new playlist is created
 void HawkPlaylistCallback::on_playlist_created(t_size p_index, const char *p_name, t_size p_name_len) {
-	in_main_thread([&](){
+	// Get string8 copy for comparisons
+	pfc::string8 new_name = p_name;
+
+	in_main_thread([=](){
 		// Get a reference to the playlist manager
 		static_api_ptr_t<playlist_manager> pm;
 
-		pm->playlist_rename(p_index, "nope", 4);
+		// Loop over the current playlists, get it's name
+		for (t_size i = 0; i < pm->get_playlist_count(); i++) {
+			pfc::string8 name;
+			pm->playlist_get_name(i, name);
+			
+			if (name == new_name) {
+				// Loop will include the one just created. If we hit it, ignore.
+				if (i == p_index) { continue; }
+
+				popup_message::g_show(name, "Playlist dupe:");
+			}
+		}
 	});
 }
